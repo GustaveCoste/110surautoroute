adf84760367b7ca30c23bc84bf36dbc9d28b740b
Gustave Coste <gustave.coste@inra.fr>
2021-09-29 12:19:06 +0200

    Temporary bugfix
    SCSS sources are compiled at each request, which is not efficient but I didn't manage to make it be compiled at deployment.

--- app/views.py
+++ app/views.py
@@ -9,11 +9,10 @@ app = Flask(__name__)
 
 app.config.from_object('config')
 
-if app.config['FLASK_ENV'] == 'development':
-    # Building CSS files at each request
-    app.wsgi_app = SassMiddleware(app.wsgi_app, {
-        'app': ('static/scss', 'static/css', '/static/css')
-    })
+# Building CSS files at each request
+app.wsgi_app = SassMiddleware(app.wsgi_app, {
+    'app': ('static/scss', 'static/css', '/static/css')
+})
 
 from app.forms import RouteForm
 from app.utils import OpenRouteServiceRouter
